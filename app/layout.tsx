7bea92f7bd6213d4edfca8978e750c99d7087274
Sjors <sjorspieter@live.nl>
2024-09-19 00:13:15 +0200

    fix: folder structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
+import "@/assets/styles/index.css";
 import type { Metadata } from "next";
 import { DM_Sans } from "next/font/google";
-import "./assets/styles/index.css";
 
 const dmSans = DM_Sans({
   subsets: ["latin"],
